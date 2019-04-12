12fa5a7fe63feeab5bfcf56c6ed0a0d4fb705324
Mark Strendin <mark.strendin@gmail.com>
2019-04-11 19:28:40 -0600

    Added a product class

---
+++ LSSDStoreFront/LSSDStoreFront-Lib/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LSSDStoreFront_Lib
+{
+    class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ThumbnailFileName { get; set; }
+
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+        public bool IsLimitedByStock { get; set; }
+        public bool IsLimitedByDate { get; set; }
+        public bool IsSpecialOrderItem { get; set; }
+        public DateTime AvailableFromDate { get; set; }
+        public DateTime AvailableToDate { get; set; }       
+
+    }
+}
