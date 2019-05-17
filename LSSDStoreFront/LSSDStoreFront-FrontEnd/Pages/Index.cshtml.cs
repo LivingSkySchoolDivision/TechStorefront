04f938df547fee2a1790cd6609dd3c952506d705
Mark Strendin <mark.strendin@lskysd.ca>
2019-05-17 19:28:37 +0000

    Merged PR 63: Mike's changes - updates to the site and navigation
    -added left navigation pulling categories from the database
    -set up table to display all items: thumbnail, name, description, price, Add to cart
    -Added search box to top navigation

--- LSSDStoreFront/LSSDStoreFront-FrontEnd/Pages/Index.cshtml.cs
+++ LSSDStoreFront/LSSDStoreFront-FrontEnd/Pages/Index.cshtml.cs
@@ -5,13 +5,34 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using LSSD.StoreFront.DB;
+using LSSD.StoreFront.Lib;
 
 namespace LSSD.StoreFront.FrontEnd.Pages
 {
     public class IndexModel : PageModel
     {
+        //public List<Product> AllProducts = new List<Product>();
+        public Inventory Inventory;
+        
         public void OnGet()
         {
+            // set up a database connection
+            string connectionString = @"data source=dc-sql2012.lskysd.ca\DCSQL2012;initial catalog=LSKYStorefront;user id=LSSDStoreFront-TEST;password=xyCTEq4tuXBEKPCRgID5dYWUxB8Yklwr;Trusted_Connection=false";
+            DatabaseContext dbConnection = new DatabaseContext(connectionString);
+            User User = new User()
+            {
+                Id = 0,
+                Username = "Test User"
+            };
+
+            Inventory = new Inventory(dbConnection);
+
+
+
+
+
+
 
         }
 
