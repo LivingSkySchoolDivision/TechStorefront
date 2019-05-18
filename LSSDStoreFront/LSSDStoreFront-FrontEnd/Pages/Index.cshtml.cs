2f98c6ceec9c3d4e647407174237488f1bd1ab97
Mark Strendin <mark.strendin@lskysd.ca>
2019-05-18 00:12:45 +0000

    Merged PR 65: Get connection string from appsettings rather than hard coding.
    Get connection string from appsettings rather than hard coding.

--- LSSDStoreFront/LSSDStoreFront-FrontEnd/Pages/Index.cshtml.cs
+++ LSSDStoreFront/LSSDStoreFront-FrontEnd/Pages/Index.cshtml.cs
@@ -12,32 +12,18 @@ namespace LSSD.StoreFront.FrontEnd.Pages
 {
     public class IndexModel : PageModel
     {
-        //public List<Product> AllProducts = new List<Product>();
         public Inventory Inventory;
         
         public void OnGet()
-        {
-            // set up a database connection
-            string connectionString = @"data source=dc-sql2012.lskysd.ca\DCSQL2012;initial catalog=LSKYStorefront;user id=LSSDStoreFront-TEST;password=xyCTEq4tuXBEKPCRgID5dYWUxB8Yklwr;Trusted_Connection=false";
-            DatabaseContext dbConnection = new DatabaseContext(connectionString);
-            User User = new User()
-            {
-                Id = 0,
-                Username = "Test User"
-            };
-
-            Inventory = new Inventory(dbConnection);
-
-
-
-
-
-
-
+        {            
         }
 
         public IndexModel(IConfiguration config)
-        {          
+        {
+            // set up a database connection            
+            DatabaseContext dbConnection = new DatabaseContext(config.GetConnectionString("InternalDatabase"));
+            Inventory = new Inventory(dbConnection);
+
         }
     }
 }
