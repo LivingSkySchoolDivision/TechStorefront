734570c1538f96ed39b13676613d8988acf8da92
Mark Strendin <mark.strendin@lskysd.ca>
2019-05-19 23:09:37 +0000

    Merged PR 67: Category and item pages and routing

---
+++ LSSDStoreFront/LSSDStoreFront-FrontEnd/Pages/Categories.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using LSSD.StoreFront.DB;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace LSSDStoreFront_FrontEnd.Pages
+{
+    public class CategoriesModel : PageModel
+    {
+        private DatabaseContext dbContext;
+        public ShoppingCart ShoppingCart;
+        public Inventory Inventory;
+
+        [BindProperty(SupportsGet = true)]
+        public string Id { get; set; }
+
+        public void OnGet()
+        {
+            
+            ShoppingCart = new ShoppingCart(dbContext, User.Identity.Name);
+        }
+
+        public CategoriesModel(IConfiguration config)
+        {
+            dbContext = new DatabaseContext(config.GetConnectionString("InternalDatabase"));
+            Inventory = new Inventory(dbContext);
+        }
+    }
+}
---
+++ LSSDStoreFront/LSSDStoreFront-FrontEnd/Pages/Items.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using LSSD.StoreFront.DB;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace LSSDStoreFront_FrontEnd.Pages
+{
+    public class ItemsModel : PageModel
+    {
+        private DatabaseContext dbContext;
+        public ShoppingCart ShoppingCart;
+        public Inventory Inventory;
+
+        [BindProperty(SupportsGet = true)]
+        public string Id { get; set; }
+
+        public void OnGet()
+        {
+            Inventory = new Inventory(dbContext);
+            ShoppingCart = new ShoppingCart(dbContext, User.Identity.Name);
+        }
+
+        public ItemsModel(IConfiguration config)
+        {
+            dbContext = new DatabaseContext(config.GetConnectionString("InternalDatabase"));
+        }
+                
+    }
+}
