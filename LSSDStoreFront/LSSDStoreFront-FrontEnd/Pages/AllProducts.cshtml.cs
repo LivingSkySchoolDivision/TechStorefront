1e22611f3c8d03044f477c4065eb5f19d29f81c5
Mike Strendin <mike.strendin@lskysd.ca>
2019-05-22 00:11:28 +0000

    Merged PR 75: Added formatting to Items page and Categories Page.
    Add Item description and price table to Items page.
    Prices show Base Price, Recycling Fee, GST, PST and Total Price.
    Added Breadcrumbs to Categories Page.
    Removed table from Index page, I put the content on new page called AllProducts for now.

---
+++ LSSDStoreFront/LSSDStoreFront-FrontEnd/Pages/AllProducts.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using LSSD.StoreFront.DB;
+using LSSD.StoreFront.Lib;
+
+namespace LSSDStoreFront_FrontEnd.Pages
+{
+    public class AllProductsModel : PageModel
+    {
+        public UserFriendlyInventory Inventory;
+
+        public void OnGet()
+        {
+        }
+
+        public AllProductsModel(IConfiguration config)
+        {
+            // set up a database connection            
+            DatabaseContext dbConnection = new DatabaseContext(config.GetConnectionString("InternalDatabase"));
+            Inventory = new UserFriendlyInventory(dbConnection);
+
+        }
+    }
+}
